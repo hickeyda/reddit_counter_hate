08496d049e346a3a56aee4afa2cfff9e6a52cd7d
JohnnyBui <johnny.bui.jb@gmail.com>
2023-03-15 12:57:09 -0700

    [model] updated

--- code/train.py
+++ code/train.py
@@ -10,8 +10,7 @@ from transformers import AutoModelForSequenceClassification
 from transformers import Trainer
 from sklearn.metrics import accuracy_score, f1_score
 
-dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-data_path = './data'
+from preprocess_data import yuetal_data_preprocess
 
 def load_data_file(path):
     lines_list = []
@@ -23,46 +22,9 @@ def load_data_file(path):
     
     return df
 
-def preprocess(df):
-    # Combine context and target
-    contexts = df['context'].values
-    targets = df['target'].values
-    df['text'] = list(map(lambda x,y: str(x) + ' [SEP] ' + str(y), contexts, targets))
-    df = df.drop(columns=['context', 'target'])
-    
-    # Cast label to int
-    df['label'] = list(map(lambda x: int(x), df['label']))
-    
-    # TODO: Binarize labels
-    
-    return df
-
-train_gold_df = load_data_file(data_path + '/gold/train.jsonl')
-val_gold_df = load_data_file(data_path + '/gold/val.jsonl')
-test_df = load_data_file(data_path + '/gold/test.jsonl') # The test set only comes from the 'gold' category
-
-train_silver_df = load_data_file(data_path + '/silver/train.jsonl')
-val_silver_df = load_data_file(data_path + '/silver/val.jsonl')
-
-#Combine Gold and Silver
-train_df = pd.concat([train_gold_df, train_silver_df])
-val_df = pd.concat([val_gold_df, val_silver_df])
-
-train_df = preprocess(train_df)
-val_df = preprocess(val_df)
-
-train_ds = Dataset.from_pandas(train_df)
-val_ds = Dataset.from_pandas(val_df)
-
-
 tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
-
 def tokenize(batch):
     return tokenizer(batch["text"], padding=True, truncation=True, add_special_tokens = True)
-
-train_encoded = train_ds.map(tokenize, batched=True, batch_size=None)
-val_encoded = train_ds.map(tokenize, batched=True, batch_size=None)
-
 
 
 def compute_metrics(pred):
@@ -72,31 +34,58 @@ def compute_metrics(pred):
     acc = accuracy_score(labels, preds)
     return {"accuracy": acc, "f1": f1}
 
+DEV = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+YU_DATA_PATH = '../reference/counter_context/data'
+RETRAIN = True
 
+def main():
+    if RETRAIN:
+        train_df = yuetal_data_preprocess(YU_DATA_PATH + '/gold/train.jsonl', 
+                                        YU_DATA_PATH + '/silver/train.jsonl')
+        val_df = yuetal_data_preprocess(YU_DATA_PATH + '/gold/val.jsonl', 
+                                        YU_DATA_PATH + '/silver/val.jsonl')
 
-model_ckpt = "roberta-base"
-model = AutoModelForSequenceClassification.from_pretrained("roberta-base", num_labels=3)
+        train_ds = Dataset.from_pandas(train_df)
+        val_ds = Dataset.from_pandas(val_df)
+        
+        
+        train_encoded = train_ds.map(tokenize, batched=True, batch_size=None)
+        val_encoded = val_ds.map(tokenize, batched=True, batch_size=None)
+        
+    else:
+        pass
+    
+    model_ckpt = "roberta-base"
+    model = AutoModelForSequenceClassification.from_pretrained("roberta-base", 
+                                                            num_labels=2)
 
-batch_size = 64
-logging_steps = len(train_encoded) // batch_size
-print(logging_steps)
-model_name = f"{model_ckpt}-finetuned-counter-hate"
-training_args = TrainingArguments(output_dir=model_name,
-                                  num_train_epochs=2,
-                                  learning_rate=2e-5,
-                                  per_device_train_batch_size=batch_size,
-                                  per_device_eval_batch_size=batch_size,
-                                  weight_decay=0.01,
-                                  evaluation_strategy="epoch",
-                                  disable_tqdm=False,
-                                  logging_steps=logging_steps,
-                                  push_to_hub=False, 
-                                  log_level="error")
+    batch_size = 4
+    logging_steps = len(train_encoded) // batch_size
+    print(logging_steps)
+    model_name = model_ckpt + "-finetune"
+    training_args = TrainingArguments(output_dir=model_name,
+                                    num_train_epochs=8,
+                                    learning_rate=2e-5,
+                                    per_device_train_batch_size=batch_size,
+                                    per_device_eval_batch_size=batch_size,
+                                    weight_decay=0.01,
+                                    evaluation_strategy="epoch",
+                                    disable_tqdm=False,
+                                    logging_steps=logging_steps,
+                                    push_to_hub=False, 
+                                    log_level="error",
+                                    save_total_limit = 2)
 
-trainer = Trainer(model=model, args=training_args, 
-                  compute_metrics=compute_metrics,
-                  train_dataset=train_encoded,
-                  eval_dataset=val_encoded,
-                  tokenizer=tokenizer)
-trainer.train()
-trainer.save_model("model")
+    trainer = Trainer(model=model, args=training_args, 
+                    compute_metrics=compute_metrics,
+                    train_dataset=train_encoded,
+                    eval_dataset=val_encoded,
+                    tokenizer=tokenizer)
+    
+    print("------------------- Training -------------------")
+    trainer.train()
+    trainer.save_model("roberta-fine-tune-model")
+
+if __name__ == "__main__":
+    main()
+    
