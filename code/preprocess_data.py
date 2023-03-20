2cf30a3fe03a7ec10a7536db773e3fd70db7bc69
JohnnyBui <johnny.bui.jb@gmail.com>
2023-03-20 11:01:50 -0700

    [model] updated data preprocessing

--- code/preprocess_data.py
+++ code/preprocess_data.py
@@ -15,7 +15,6 @@ DEV = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 MODEL_CKPT = "roberta-base"
 TOKENIZER = RobertaTokenizer.from_pretrained(MODEL_CKPT)
 BASE_MODEL = AutoModel.from_pretrained(MODEL_CKPT).to(DEV)
-YU_DATA_PATH = '../reference/counter_context/data'
 
 def load_data_file(path):
     lines_list = []
@@ -39,6 +38,33 @@ def upsample(df):
                                     random_state=123) # reproducible results
     
     return pd.concat([majority_df, upsampled_minority_df])
+
+YU_DATA_PATH = '../reference/counter_context/data'
+def data_preprocess(data_set, file_name, file_type, upsampling):
+    """
+    gold_data_path: path to gold jsonl data file
+    silver_data_path: path to silver jsonl data file
+    """
+    df = load_data_file(YU_DATA_PATH + "/" + data_set + "/" + file_name + "." + file_type)
+    
+    if upsampling:
+        file_name = "upsample_" + file_name
+    
+    # Binarize and cast label to int
+    df['label'] = list(map(lambda x: 0 if int(x) == 1 or int(x) == 0 else 1, df['label']))
+    
+    if upsampling:
+        df = upsample(df)
+    
+    # Tokenize
+    ds = Dataset.from_pandas(df)
+    tokens = ds.map(tokenize, batched=True, batch_size=None)
+    torch.save(tokens, data_set + '_' + file_name + "_tokens.pt")
+    
+    # Extract hidden states
+    tokens.set_format("torch", columns=["input_ids", "attention_mask", "label"])
+    hiddens = tokens.map(extract_hidden_states, batched=True, batch_size=32)
+    torch.save(hiddens, data_set + '_' + file_name + "_hidden.pt")
 
 def yuetal_data_preprocess(gold_data_path, silver_data_path):
     """
@@ -76,36 +102,29 @@ def tokenize(batch):
 
 
 def main():
-    if not os.path.exists("./train_hidden.pt") or not os.path.exists("./val_hidden.pt"):
-        train_df = yuetal_data_preprocess(YU_DATA_PATH + '/gold/train.jsonl', 
-                                        YU_DATA_PATH + '/silver/train.jsonl')
-        val_df = yuetal_data_preprocess(YU_DATA_PATH + '/gold/val.jsonl', 
-                                        YU_DATA_PATH + '/silver/val.jsonl')
-
-        train_df = upsample(train_df)
+    settings = [
+        ("gold", "train", False),   # data set, file name, upsample
+        ("gold", "train", True),
+        ("gold", "val", False),
+        ("silver", "train", False),
+        ("silver", "train", True),
+        ("silver", "val", False),
+    ]
+    
+    file_names = []
+    for setting in settings:
+        fn = setting[0] + "_" + setting[1]
+        if setting[2]:
+            fn = "upsample_" + fn
+        file_names.append(fn + "_hidden.pt")
+        file_names.append(fn + "_tokens.pt")
         
-        train_ds = Dataset.from_pandas(train_df)
-        val_ds = Dataset.from_pandas(val_df)
-        
-        train_encoded = train_ds.map(tokenize, batched=True, batch_size=None)
-        val_encoded = val_ds.map(tokenize, batched=True, batch_size=None)
-        
-        # Save the hidden state
-        torch.save(train_encoded, "train_encoded.pt")
-        torch.save(val_encoded, "val_encoded.pt")
-        
-        train_encoded.set_format("torch", columns=["input_ids", "attention_mask", 
-                                                "label"])
-        val_encoded.set_format("torch", columns=["input_ids", "attention_mask", 
-                                                "label"])
-        
-        train_hidden = train_encoded.map(extract_hidden_states, batched=True, batch_size=32)
-        val_hidden = val_encoded.map(extract_hidden_states, batched=True, batch_size=32)
-
-        # Save the hidden state
-        torch.save(train_hidden, "train_hidden.pt")
-        torch.save(val_hidden, "val_hidden.pt")
-
+    for fn in file_names:
+        if not os.path.exists(fn):
+            for setting in settings:
+                data_preprocess(setting[0], setting[1], "jsonl", setting[2])
+            break
+    
 if __name__ == "__main__":
     main()
     
